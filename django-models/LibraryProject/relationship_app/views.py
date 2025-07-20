49cb357a8c91fea4fd31ca2a1df4f7fbe327d35b
unknown <yusuff4u2c@gmail.com>
2025-07-20 22:52:03 +0100

    relationship2

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,22 +1,7 @@
 from django.shortcuts import render, redirect
-from django.views.generic.detail import DetailView
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
-
-from .models import Library
-from .models import Book
-
-
-def list_books(request):
-    books = Book.objects.all()
-    return render(request, 'relationship_app/list_books.html', {'books': books})
-
-
-class LibraryDetailView(DetailView):
-    model = Library
-    template_name = 'relationship_app/library_detail.html'
-    context_object_name = 'library'
 
 
 def user_login(request):
@@ -25,14 +10,16 @@ def user_login(request):
         if form.is_valid():
             user = form.get_user()
             login(request, user)
-            return redirect('list_books')  # or any page you want after login
+            return redirect('list_books')  # Redirect to any valid view after login
     else:
         form = AuthenticationForm()
     return render(request, 'relationship_app/login.html', {'form': form})
 
+
 def user_logout(request):
     logout(request)
     return render(request, 'relationship_app/logout.html')
+
 
 def user_register(request):
     if request.method == "POST":
