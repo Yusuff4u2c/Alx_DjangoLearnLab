e5d64008fee1abbd3c01b291d96252f5f1dd3d6d
unknown <yusuff4u2c@gmail.com>
2025-07-27 16:33:14 +0100

    advanced features 1

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -12,3 +12,15 @@ class BookForm(forms.ModelForm):
         if "<script>" in title.lower():
             raise forms.ValidationError("Invalid characters in title.")
         return title
+
+
+class ExampleForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'publication_year']
+
+    def clean_title(self):
+        title = self.cleaned_data.get('title')
+        if "<script>" in title.lower():
+            raise forms.ValidationError("Invalid characters in title.")
+        return title
