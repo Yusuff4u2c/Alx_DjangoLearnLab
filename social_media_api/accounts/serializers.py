fc35d32fb4671f580842cbf43f61970673b9e690
unknown <yusuff4u2c@gmail.com>
2025-08-24 22:32:53 +0100

    social media api

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,12 +1,11 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
-from rest_framework.authtoken.models import Token
 
 User = get_user_model()
 
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
-    # Explicit password field with write_only
+    # 👇 This is what the error says is missing
     password = serializers.CharField(write_only=True)
 
     class Meta:
@@ -14,12 +13,9 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         fields = ("id", "username", "email", "password")
 
     def create(self, validated_data):
-        # Use create_user to hash the password properly
-        user = get_user_model().objects.create_user(
+        # Use create_user to properly hash the password
+        return User.objects.create_user(
             username=validated_data["username"],
             email=validated_data.get("email"),
             password=validated_data["password"],
         )
-        # Create auth token for the new user
-        Token.objects.create(user=user)
-        return user
